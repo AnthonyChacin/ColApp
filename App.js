8d678c50ad1ad21571d28b43bfacf2a32edfb9af
AnthonyChacin <anthony.chacin@correo.unimet.edu.ve>
2019-05-14 20:37:58 -0400

    first commit!

--- App.js
+++ App.js
@@ -21,8 +21,8 @@ export default class App extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Te amo Arantxa!</Text>
-        <Text style={styles.instructions}>ColApp proximamente... </Text>
+        <Text style={styles.welcome}>Welcome to ColApp!</Text>
+        <Text style={styles.instructions}>Proximamente... </Text>
         <Text style={styles.instructions}>{instructions}</Text>
       </View>
     );
