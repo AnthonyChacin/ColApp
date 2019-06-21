5ba7f4a9756e486c2d85c24d590d6a9ee886e535
Arantxa García <arantxa.garcia@correo.unimet.edu.ve>
2019-06-21 18:38:04 -0400

    No se renderiza el componente de la lista

--- src/components/historialPasajero.js
+++ src/components/historialPasajero.js
@@ -3,10 +3,13 @@ import {
     StyleSheet,
     ActivityIndicator,
     Dimensions,
-    FlatList
+    FlatList,
+    View,
+    Text
 } from 'react-native';
-import { Container, View } from 'native-base';
-import { List, ListItem } from 'react-native-elements'
+import { ListItem } from 'react-native-elements'
+import MapView from 'react-native-maps';
+import moment, { isMoment } from 'moment';
 
 const { width, height } = Dimensions.get('window')
 const halfHeight = height / 3
@@ -20,15 +23,12 @@ class HistorialPasajero extends React.Component {
         this.state = {
             loading: true,
             historial: [
-                {
-                    "destino": "UNIMET",
-                    "hora": "15-06-2019 3:00 p.m."
-                },
-                {
-                    "destino": "SANTA FE",
-                    "hora": "16-06-2019 2:00 p.m."
-                }
-            ]
+
+                { "_id": "5d0cdcd9d8211a0017f0e14e", "origen": { "latitude": 10.460896, "longitude": -66.866109, "latitudeDelta": 0.01, "longitudeDelta": 0.005620608899297424 }, "destino": "Unimet", "tarifa": "6000", "banco": "Provincial", "hora": "2019-06-27T07:00:00-04:30", "cantPasajeros": "1", "vehiculo": "Carro", "estado": "Pedida", "pasajero": "5d091bee68ab9f0017cb51f9", "creacionCola": "2019-06-21T09:35:55-04:30" },
+                { "_id": "5d0d1b4373a65300170aa001", "origen": { "latitude": 10.3998821, "longitude": -66.8865553, "latitudeDelta": 0.01, "longitudeDelta": 0.005625 }, "destino": "Unimet", "tarifa": "2450", "banco": "Mercantil", "hora": "2019-06-21T16:00:00-04:00", "cantPasajeros": "1", "vehiculo": "Moto", "estado": "Pedida", "pasajero": "5d010180e280a601a8314e76", "creacionCola": "2019-06-21T14:00:34-04:00" },
+                { "_id": "5d0cd85bd8211a0017f0e14d", "origen": { "latitude": 10.460896, "longitude": -66.866109, "latitudeDelta": 0.01, "longitudeDelta": 0.005620608899297424 }, "destino": "Unimet", "tarifa": "1000", "banco": "Provincial", "hora": "2019-06-27T07:00:00-04:30", "cantPasajeros": "1", "vehiculo": "Carro", "estado": "Pedida", "pasajero": "5d091bee68ab9f0017cb51f9", "creacionCola": "2019-06-21T09:16:44-04:30" }
+            ],
+            selected: null
 
 
         }
@@ -38,33 +38,78 @@ class HistorialPasajero extends React.Component {
 
     }
 
+    _itemSelected(selection) {
+        this.setState({
+            selected: selection
+        })
+    }
+
     render() {
-
-        return (
-            <Container>
-                {!this.state.loading && (
-                    <View style={styles.container}>
-                        <ActivityIndicator size='large' color="orange" style={{ padding: 20 }} />
+        if (!this.state.loading) {
+            return (
+                <View style={styles.container}>
+                    <ActivityIndicator size='large' color="orange" style={{ padding: 20 }} />
+                </View>
+            )
+        } else if (this.state.selected !== null) {
+            return (
+                <View style={{ height: (HEIGHT * 0.9), marginTop: 0 }}>
+                    <View style={styles.Container}>
+                        <MapView style={styles.map}
+                            region={{
+                                latitude: this.state.selected.origen.latitude,
+                                longitude: this.state.selected.origen.longitude,
+                                latitudeDelta: this.state.selected.origen.latitudeDelta,
+                                longitudeDelta: this.state.selected.origen.longitudeDelta
+                            }}>
+                            <MapView.Marker
+                                coordinate={{
+                                    latitude: this.state.selected.origen.latitude,
+                                    longitude: this.state.selected.origen.longitude
+                                }}>
+                            </MapView.Marker>
+                        </MapView>
                     </View>
-                )}
-                {!!this.state.historial && (
-                    <View>
-                        <List>
-                            <FlatList
-                                data={this.state.historial}
-                                renderItem={({ item }) => (
-
-                                    <ListItem
-                                        title={item.destino}
-                                        subtitle={item.hora}
-                                    />
-                                )}
-                            />
-                        </List>
+                    <Text note style={{ marginLeft: 10, height: 20, marginTop: 0 }}>Punto de encuentro</Text>
+                    <View style={{ height: 20 }}>
+                        <Text note style={{ marginLeft: 20 }}>Tarifa: {this.state.selected.tarifa} Bs.</Text>
                     </View>
-                )}
-            </Container>
-        )
+
+                    <View style={{ height: 20 }}>
+                        <Text note style={{ marginLeft: 20 }}>Fecha y Hora: {moment(`${this.state.selected.hora}`).format('DD-MM-YYYY, hh:mm a')}</Text>
+                    </View>
+
+                    <View style={{ height: 20 }}>
+                        <Text note style={{ marginLeft: 20 }}>Vehículo: {this.state.selected.vehiculo}</Text>
+                    </View>
+
+                    <View style={{ height: 20 }}>
+                        <Text note style={{ marginLeft: 20 }}>Banco: {this.state.selected.banco}</Text>
+                        <Text></Text>
+                    </View>
+
+                    <View style={{ height: 20 }}>
+                        <Text note style={{ marginLeft: 20 }}>Cantidad de Pasajeros: {this.state.selected.cantPasajeros}</Text>
+                    </View>
+                </View>
+
+
+            )
+        } else {
+            return (
+                <FlatList
+                    data={this.state.historial}
+                    renderItem={({ item }) =>
+
+                        <ListItem
+                            title={item.destino}
+                            subtitle={item.hora}
+                            onPress={this._itemSelected(item)}
+                        />
+                    }
+                />
+            )
+        }
     }
 }
 
@@ -84,5 +129,11 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         backgroundColor: 'rgb(20,20,20)',
         ...StyleSheet.absoluteFillObject,
+    },
+    map: {
+        left: 0,
+        right: 0,
+        ...StyleSheet.absoluteFillObject,
+        flex: 1
     }
 })
